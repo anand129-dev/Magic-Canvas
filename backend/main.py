fa016a3a56db50f4724bdaa9f7e171d5ce4ec327
Anand <anand@git.system>
2024-12-11 19:41:19 +0530

    Update main.py and add requirements.txt for backend

--- backend/main.py
+++ backend/main.py
@@ -28,6 +28,7 @@ async def health():
 
 app.include_router(calculator_router, prefix='/calculate', tags=['calculate'])
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host=SERVER_URL, port=int(PORT), reload=(ENV == 'dev'))
+# Use the below only for local machine (Its not for deployment)
+# if __name__ == '__main__':
+#     uvicorn.run('main:app', host=SERVER_URL, port=int(PORT), reload=(ENV == 'dev'))
 
